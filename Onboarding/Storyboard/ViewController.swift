7868f6b9e6e54727f0870ba61a461580370e2ea1
kostiantynkaniuka@gmail.com <heyhocombo@gmail.com>
2025-04-06 11:08:41 +0200

    initial commit / adding snapkit

---
+++ Onboarding/Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Kostiantyn Kaniuka on 06.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
